b69095cfd1476e08d854d34c1766884d960cfef1
dp <dpcode@outlook.com>
2017-10-05 22:53:00 +0200

    Commit interfaces

---
+++ CodeGraph.Interfaces/Interfaces.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+
+namespace CodeGraph.Interfaces
+{
+    public interface ICallGraph
+    {
+        List<Node> Nodes { get; set; }
+    }
+
+    public class Node
+    {
+        public List<Node> Incoming { get; set; }
+
+        public List<Node> Outgoing { get; set; }
+
+        public ISymbol Symbol { get; set; }
+
+        public Dictionary<string, string> PropertyBag { get; set; }
+    }
+
+}
