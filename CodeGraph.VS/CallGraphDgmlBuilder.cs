e298c901761b6b2f05669dccd5f8e31006377115
dp <dpcode@outlook.com>
2017-10-06 00:37:16 +0200

    Added graph buildr framework

---
+++ CodeGraph.VS/CallGraphDgmlBuilder.cs
@@ -0,0 +1,39 @@
+﻿using CodeGraph.Interfaces;
+using Microsoft.VisualStudio.GraphModel;
+
+namespace CodeGraph.VS
+{
+    internal class CallGraphDgmlBuilder
+    {
+        public static Graph Create(ICallGraph callGraph)
+        {
+            CallGraphDgmlBuilder builder = new CallGraphDgmlBuilder();
+            Graph g = builder.CreateDiagram(callGraph);
+            return g;
+        }
+        
+        private CallGraphDgmlBuilder()
+        {
+            // private constructor
+        }
+
+        protected Graph CreateDiagram(ICallGraph model)
+        {
+            //if (model == null)
+            //{
+            //    return null;
+            //}
+
+            Graph graph = this.CreateGraph();
+            return graph;
+        }
+
+        private Graph CreateGraph()
+        {
+            // TODO: schema
+            Graph g = new Graph();
+            return g;
+        }
+
+    }
+}
