c60f1fa4b10275001d6267895f247544d4ec0900
dp <dpcode@outlook.com>
2017-10-06 12:05:32 +0200

    Linked up code

---
+++ CodeGraph/CallGraph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeGraph.Interfaces;
+
+namespace CodeGraph
+{
+    public class CallGraph : ICallGraph
+    {
+        public List<Node> Nodes { get; set; } = new List<Node>();
+    }
+}
