ba354b88942f81747e809f78dd94b2e56cb4f5be
dp <dpcode@outlook.com>
2017-10-06 09:39:22 +0200

    Added graph builder

---
+++ CodeGraph.VS/Progression/ObjectMapper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace CodeGraph.VS.Progression
+{
+    /// <summary>
+    /// Maintains a mapping between pairs of objects
+    /// </summary>
+    public class ObjectMapper<TSource, TTarget>
+    {
+        private readonly Dictionary<TSource, TTarget> sourceToTargetMap;
+
+        public ObjectMapper()
+        {
+            this.sourceToTargetMap = new Dictionary<TSource, TTarget>();
+        }
+
+        public void Register(TSource source, TTarget target)
+        {
+            this.sourceToTargetMap.Add(source, target);
+        }
+
+        public TTarget TryGetTarget(TSource source)
+        {
+            TTarget target;
+            this.sourceToTargetMap.TryGetValue(source, out target);
+            return target;
+        }
+
+        public T TryGetTarget<T>(TSource source) where T: class, TTarget
+        {
+            TTarget target;
+            this.sourceToTargetMap.TryGetValue(source, out target);
+            T t = target as T;
+            return t;
+        }
+
+    }
+}
