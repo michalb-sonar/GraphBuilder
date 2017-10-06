f8247ff1c2db46e4eba2066c114045666895d41e
dp <dpcode@outlook.com>
2017-10-06 11:17:52 +0200

    Added test

--- tests/CodeGraph.VS.Tests/CallGraphDgmlBuilderTests.cs
+++ tests/CodeGraph.VS.Tests/CallGraphDgmlBuilderTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CodeGraph.Interfaces;
 using CodeGraph.VS.Progression;
+using CodeGraph.VS.Tests.Helpers;
 using Microsoft.VisualStudio.GraphModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,10 +11,30 @@ namespace CodeGraph.VS.Tests
     public class CallGraphDgmlBuilderTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CreateSimpleGraph()
         {
-            ICallGraph callGraph = null;
+            ICallGraph callGraph = new MockCallGraph();
+
+            Node n1 = new Node();
+            Node n2 = new Node();
+
+            n1.Incoming = new System.Collections.Generic.List<Node>()
+            {
+                n2
+            };
+
+            n2.Outgoing = new System.Collections.Generic.List<Node>() { n1 };
+
+            callGraph.Nodes.Add(n1);
+            callGraph.Nodes.Add(n2);
+
             Graph g = CallGraphDgmlBuilder.Create(callGraph);
+        }
+
+        private Node CreateNode()
+        {
+            Node n = new Node();
+            return n;
         }
     }
 }
