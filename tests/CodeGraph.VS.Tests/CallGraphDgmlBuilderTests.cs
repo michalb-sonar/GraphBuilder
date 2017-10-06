9f61d7ab00fb3245b4e3746081c454a4e4905b47
dp <dpcode@outlook.com>
2017-10-06 09:58:47 +0200

    Added test project

---
+++ tests/CodeGraph.VS.Tests/CallGraphDgmlBuilderTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CodeGraph.Interfaces;
+using CodeGraph.VS.Progression;
+using Microsoft.VisualStudio.GraphModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeGraph.VS.Tests
+{
+    [TestClass]
+    public class CallGraphDgmlBuilderTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ICallGraph callGraph = null;
+            Graph g = CallGraphDgmlBuilder.Create(callGraph);
+        }
+    }
+}
